9b00f1f1c9547578e064263a801278c42dd600ad
Eamonn O'Brien-Strain <eobrain@gmail.com>
2023-03-29 04:52:39 +0000

    First working version

--- index.js
+++ index.js
@@ -1,40 +1,40 @@
-let capture;
+let capture
 
 const WIDTH = 1280 / 2
 const HEIGHT = 960 / 2
 
-
-function setup() {
-    createCanvas(WIDTH, HEIGHT);
-    capture = createCapture(VIDEO);
-    capture.size(WIDTH, HEIGHT);
-    //capture.hide();
+function setup () {
+  createCanvas(WIDTH, HEIGHT)
+  capture = createCapture(VIDEO)
+  capture.size(WIDTH, HEIGHT)
+  capture.hide()
 }
 
 const history = new Array(HEIGHT)
 let frame = 0
 
-function draw() {
-    background(255);
-    image(capture, 0, 0, WIDTH, HEIGHT);
+function draw () {
+  background(255)
+  image(capture, 0, 0, WIDTH, HEIGHT)
 
-    loadPixels();
+  loadPixels()
 
-    history[frame % HEIGHT] = pixels
+  history[frame % HEIGHT] = pixels.slice()
 
-    if (frame > HEIGHT) {
-        for (let y = 0; y < height; y++) {
-            prevPixels = history[y]
-            const lineOffset = y * width * 4
-            for (let x = 0; x < width; x++) {
-                const pixelOffset = lineOffset + x * 4
-                for (let channel = 0; channel < 4; channel++) {
-                    const i = pixelOffset + channel
-                    pixels[i] = prevPixels[i]
-                }
-            }
+  if (frame > HEIGHT) {
+    for (let y = 0; y < height; y++) {
+      const prevPixels = history[(frame + y) % HEIGHT]
+      const lineOffset = y * width * 4
+      for (let x = 0; x < width; x++) {
+        const pixelOffset = lineOffset + x * 4
+        for (let channel = 0; channel < 4; channel++) {
+          const i = pixelOffset + channel
+          pixels[i] = prevPixels[i]
         }
+      }
     }
-    updatePixels();
-    ++frame;
+  }
+  updatePixels()
+  ++frame
+  $frame.innerText = `${frame}`
 }
