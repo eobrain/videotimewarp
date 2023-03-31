09b055572333b13b2a7754b4eeace5daf12c5674
Eamonn O'Brien-Strain <eob@well.com>
2023-03-30 21:09:06 -0700

    Handle pixel scaling on phones

--- index.js
+++ index.js
@@ -6,9 +6,9 @@
     const constraints = {
       audio: false,
       video: {
-        width: 400,
-        height: 400
-      }
+        width: 256,
+        height: 256
+      },
     }
 
     const stream = await navigator.mediaDevices.getUserMedia(constraints)
@@ -18,22 +18,26 @@
     return {
       cameraWidth: streamSettings.width,
       cameraHeight: streamSettings.height
-    }
+    };
   }
 
   const { cameraWidth, cameraHeight } = await getCameraSize()
 
   const s = (p) => {
     let capture
+    let d
+    let dw
 
     p.setup = function () {
       p.createCanvas(cameraWidth, cameraHeight)
       capture = p.createCapture(p.VIDEO)
       capture.size(cameraWidth, cameraHeight)
       capture.hide()
-    }
+      d = p.pixelDensity()
+      dw = d * p.width
+    };
 
-    const AGE = 100
+    const AGE = cameraHeight
     const history = new Array(AGE)
     let frame = 0
 
@@ -47,13 +51,19 @@
 
       if (frame > AGE) {
         for (let y = 0; y < p.height; y++) {
-          const prevPixels = history[Math.trunc(frame + y * AGE / cameraHeight) % AGE]
-          const lineOffset = y * p.width * 4
+          const prevPixels =
+            history[Math.trunc(frame + (y * AGE) / cameraHeight) % AGE]
+          const subpixelY0 = y * d
           for (let x = 0; x < p.width; x++) {
-            const pixelOffset = lineOffset + x * 4
-            for (let channel = 0; channel < 4; channel++) {
-              const i = pixelOffset + channel
-              p.pixels[i] = prevPixels[i]
+            const subpixelX0 = x * d
+            for (let j = 0; j < d; j++) {
+              const subpixelOffset = (subpixelY0 + j) * dw
+              for (let i = 0; i < d; i++) {
+                const index = 4 * (subpixelOffset + subpixelX0 + i)
+                for (let channel = 0; channel < 4; channel++) {
+                  p.pixels[index + channel] = prevPixels[index + channel]
+                }
+              }
             }
           }
         }
@@ -63,10 +73,10 @@
       $size.innerText = `${cameraWidth}x${cameraHeight}`
       $frame.innerText = `${frame}`
       $fps.innerText = `${p.frameRate().toFixed(2)}`
-    }
+    };
   }
 
   new p5(s, '$p5')
-  defaultCanvas0.style.height = 'auto'
-  defaultCanvas0.style.width = '90vmin'
+  defaultCanvas0.style.height = 'auto';
+  defaultCanvas0.style.width = '90vmin';
 })()
